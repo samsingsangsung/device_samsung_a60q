c74c5ea052575fbdbef9ce7eb2760cf1cfd0f3dc
Smasung <jannatulferdousy101327@gmail.com>
2025-12-02 21:02:03 +0600

    Update omni_a60q.mk

--- omni_a60q.mk
+++ omni_a60q.mk
@@ -22,16 +22,16 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from a60q device
-$(call inherit-product, device/samsung/a60q/device.mk)
+# Inherit from m40 device
+$(call inherit-product, device/samsung/m40/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := a60q
-PRODUCT_NAME := omni_a60q
+PRODUCT_DEVICE := m40
+PRODUCT_NAME := omni_m40
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := samsung
 PRODUCT_MANUFACTURER := samsung
